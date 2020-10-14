90147781f7bed671cc32c32de1b27ef4c7e78fd8
yiwengao <yiwen.gao@columbia.edu>
2020-10-14 00:15:29 -0500

    add template test file

---
+++ yiwen-s-first-extension/out/test/suite/temp.test.js
@@ -0,0 +1,31 @@
+function testDetectLocationStack() {
+    /** 
+     * if press cmd + click on function, add clicked line to stack
+     * print stack and make sure it's equal to the mock
+    */
+}
+
+function testKeyBindingStack() {
+    /**
+     * if press ctrl + shift + 1 (or some other key binding), pop line from stack
+     * print stack and make sure it's equal to the mock
+     */
+}
+
+function testKeyBinding() {
+    /**
+     * if press ctrl + shift + 1 (or some other key binding), navigate user back to previous location
+     */
+}
+
+function testInitializationStack() {
+    /**
+     * make sure stack is empty on VSCode startup
+     */
+}
+
+function testInitialization() {
+    /**
+     * make sure nothing happens if press ctrl + shift + 1 on VSCode startup
+     */
+}
