16059364afcb6d69f2a2c37ab4ee705201d87d40
yiwengao <yiwen.gao@columbia.edu>
2020-11-04 00:37:00 -0600

    add initial logic for code navigation

--- yiwen-s-first-extension/out/extension.js
+++ yiwen-s-first-extension/out/extension.js
@@ -4,21 +4,52 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    var stack = [];
+
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "yiwen-s-first-extension" is now active!');
+    console.log('Congratulations, your extension is now active!');
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('yiwen-s-first-extension.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from Yiwen&#39;s First Extension!');
+    let addLocation = vscode.commands.registerCommand('extension.addLocation', () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            return;
+        }
+
+        vscode.commands.executeCommand(
+            'vscode.executeDefinitionProvider', 
+            activeEditor.document.uri, 
+            activeEditor.selection.active
+        ).then((locations) => {
+            const curr = locations[0];
+            stack.push(curr);
+            console.log('curr pos:', curr);
+            return vscode.commands.executeCommand('editor.action.goToDefinition');
+            // return vscode.commands.executeCommand('vscode.open', curr.uri);
+        // }).then(() => {
+        //     const curr = stack[stack.length - 1];
+        //     return vscode.commands.executeCommand('cursorMove', curr.uri, curr.range);
+        // }).then(() => {
+
+        }).catch(err => console.log(err));
     });
-    context.subscriptions.push(disposable);
+
+    let removeLocation = vscode.commands.registerCommand('extension.removeLocation', () => {
+        if (locations.length > 0) {
+            const curr = locations.pop();
+            console.log(`remove location ${curr}`);
+        }
+    });
+
+    context.subscriptions.push(addLocation);
+    context.subscriptions.push(removeLocation);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- yiwen-s-first-extension/src/extension.ts
+++ yiwen-s-first-extension/src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Yiwen&#39;s First Extension!');
+		vscode.window.showInformationMessage('hiii :)!');
 	});
 
 	context.subscriptions.push(disposable);
